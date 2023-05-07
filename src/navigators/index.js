c2b69a77495b2778ebd0a5cb81f99979cfd1da3d
Carmen <uo264637@uniovi.es>
2023-05-07 14:03:48 +0200

    Half way

---
+++ src/navigators/index.js
@@ -0,0 +1,11 @@
+import { NavigationContainer } from "@react-navigation/native";
+
+import { AppNavigator } from "./app.navigator";
+
+export const Navigator = () => {
+  return (
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
+  );
+};
